a67d23e99b2d434ca341a3111ad577bcacdcc306
Youta EGUSA <y-egusa@sakura.ad.jp>
2016-05-24 21:51:38 +0900

    Moving libraries out of internal repository.

---
+++ examples/SakuraAlpha_I2C/SakuraAlpha_I2C.ino
@@ -0,0 +1,66 @@
+#include <SakuraAlpha.h>
+
+#define BUF_LEN 16
+
+uint32_t cnt = 0;
+
+SakuraAlphaI2C sakura;
+
+void setup() {
+  Serial.begin(9600);  // start serial for output
+}
+
+uint8_t updated[BUF_LEN];
+
+void loop() {
+  cnt++;
+  Serial.println(cnt);
+  sakura.writeChannel(0,cnt);
+  sakura.writeChannel(1,cnt);
+  sakura.writeChannel(2,cnt);
+  delay(250);
+
+  int num_updated = sakura.getUpdatedChannels(updated, BUF_LEN);
+  Serial.print(num_updated);
+  Serial.print(" updated channels: ");
+  for (int i = 0; i < num_updated; i++) {
+    Serial.print(' ');
+    Serial.print((int)updated[i]);
+  }
+  Serial.println("");
+  
+  char type=0;
+  byte value[8]={0};
+  
+  sakura.readChannel(0, &type, value);
+
+  for (int i = 0; i < 8; i++) {
+    Serial.print(' ');
+    Serial.print(value[i],HEX);
+  }
+
+  Serial.print(" type:");
+  Serial.print(type);
+  Serial.println("");
+  delay(250);
+
+  Serial.print("Tx[0] status: ");
+  Serial.print( sakura.getTxChannelStatus(0) );
+  Serial.println("");
+
+  sakura.transmit(TRANSMIT_ONCE);
+  delay(250);
+
+  num_updated = sakura.getUntransmittedChannels(updated, BUF_LEN);
+  Serial.print(num_updated);
+  Serial.print(" untransmitted channels: ");
+  for (int i = 0; i < num_updated; i++) {
+    Serial.print(' ');
+    Serial.print((int)updated[i]);
+  }
+  Serial.println("");
+  delay(1000);
+
+  Serial.println("");
+}
+
